6d9592760e63f98d4e4193cd7ad2a2483e7c1090
Christian Garrovillo <me@christiang.dev>
2021-03-17 14:40:15 -0600

    Authorizer function

--- api/src/controllers/v1/checkout/createCheckoutSession.ts
+++ api/src/controllers/v1/checkout/createCheckoutSession.ts
@@ -1,5 +1,4 @@
 import { Context } from 'koa'
-
 import { stripe } from '../../../util/stripe'
 import { CONSTANTS } from '../../../util/constants'
 import type { CartItem, Checkout } from '../../../types/usc'
@@ -18,27 +17,22 @@ const createCheckoutSession = async (ctx: Context) => {
     // Turn the cart_items into line_items
     const line_items = await processCartItems(cart)
 
-    const session = await stripe.checkout.sessions.create(
-      {
-        allow_promotion_codes: true,
-        billing_address_collection: 'auto',
-        expand: ['line_items'],
-        mode: 'payment',
-        payment_method_types: ['card'],
-        // @ts-ignore
-        shipping_rates: ['shr_1ITJsNJe4oNYc3QvZnoE62W8'],
-        shipping_address_collection: {
-          allowed_countries: ['US', 'CA'],
-        },
-        line_items: line_items,
-        submit_type: 'pay',
-        success_url: `${CONSTANTS.URLS.STORE_DOMAIN}?success=true`,
-        cancel_url: `${CONSTANTS.URLS.STORE_DOMAIN}?canceled=true`,
+    const session = await stripe.checkout.sessions.create({
+      allow_promotion_codes: true,
+      billing_address_collection: 'auto',
+      expand: ['line_items'],
+      mode: 'payment',
+      payment_method_types: ['card'],
+      // @ts-ignore
+      shipping_rates: ['shr_1ITJsNJe4oNYc3QvZnoE62W8'],
+      shipping_address_collection: {
+        allowed_countries: ['US', 'CA'],
       },
-      {
-        // TODO: Indempotency
-      }
-    )
+      line_items: line_items,
+      submit_type: 'pay',
+      success_url: `${CONSTANTS.URLS.STORE_DOMAIN}?success=true`,
+      cancel_url: `${CONSTANTS.URLS.STORE_DOMAIN}?canceled=true`,
+    })
 
     // Return with session id
     ctx.body = session.id
--- api/src/util/Stripe.ts
+++ api/src/util/Stripe.ts
@@ -2,6 +2,8 @@ import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_RESTRICTED_KEY || '', {
   apiVersion: '2020-08-27',
+  typescript: true,
+  maxNetworkRetries: 3,
 })
 
 export { stripe }
