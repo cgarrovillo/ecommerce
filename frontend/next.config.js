3d92d939c8b371f445262f1a94a4bd93ec89338a
Christian Garrovillo <me@christiang.dev>
2021-03-17 18:23:26 -0600

    Rollback usc to 2.4.3

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,10 @@
-module.exports = {
+// Next Bundle Analyzer to analyze dependencies
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
   images: {
     domains: ['files.stripe.com'],
   },
-}
+})
