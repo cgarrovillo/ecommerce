7a32da20af372df51b71bf9d8f62294e02754956
Christian Garrovillo <me@christiang.dev>
2021-03-23 18:41:17 -0600

    add strict mode to next config, logo

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,9 +3,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withBundleAnalyzer({
-  target: 'serverless',
+module.exports = {
   images: {
     domains: ['files.stripe.com'],
   },
-})
+  reactStrictMode: true,
+}
